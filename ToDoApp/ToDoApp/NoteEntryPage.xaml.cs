3a819b56d0019807982b24f831ca0c64430713ee
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-08-23 23:48:29 -0700

    write OnSaveButtonClicked method in NoteEntryPage.xaml.cs file

--- ToDoApp/ToDoApp/NoteEntryPage.xaml.cs
+++ ToDoApp/ToDoApp/NoteEntryPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ToDoApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,20 @@ namespace ToDoApp
         public NoteEntryPage()
         {
             InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if(string.IsNullOrWhiteSpace(note.Filename))
+            {
+               var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName().notes.txt}");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                File.WriteAllText(note.Filename, note.Text);
+            }
         }
     }
 }
