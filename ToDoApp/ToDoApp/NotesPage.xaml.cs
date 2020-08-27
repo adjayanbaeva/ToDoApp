c9689540d4ee7433893fcaaee7812faca69471cb
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2020-08-27 00:00:35 -0700

    create a list of notes and add data about each note

--- ToDoApp/ToDoApp/NotesPage.xaml.cs
+++ ToDoApp/ToDoApp/NotesPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ToDoApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,23 @@ namespace ToDoApp
         public NotesPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach(var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date=File.GetCreationTime(filename)
+                });
+            }
         }
 
         private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
